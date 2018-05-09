4733a2cb2da70019ca08b06342acf4152c363a82
Jennifer Ponder <jennp@Jennifers-MacBook-Pro.local>
2018-05-09 19:23:47 -0400

    updated color scheme for background of home page and survey

--- routing/htmlRoutes.js
+++ routing/htmlRoutes.js
@@ -4,7 +4,6 @@ var path = require("path");
 
 module.exports = function(app) {
     app.get("/", function (req, res) {
-        //console.log(path.join(__dirname, "../public/home.html"));
         res.sendFile(path.join(__dirname, "../public/home.html"));
     });
     app.get("/survey", function (req, res) {
